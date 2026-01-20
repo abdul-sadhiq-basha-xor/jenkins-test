74652a4da9040be6f6aaab27477930244a19a91c
abdul-sadhiq-basha-xor <Abdul.Basha@Xoriant.Com>
2026-01-20 10:32:18 +0000

    initial commit

---
+++ src/main/java/com/jenkins/controller/Jenkins.java
@@ -0,0 +1,15 @@
+package com.jenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Jenkins {
+	
+	
+	@GetMapping
+	public String getMethod() {
+		return "Hello jenkins";	
+	}
+
+}
